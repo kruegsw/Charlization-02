b0279563a7aa6978392252b68434e3a329f87374
kruegsw <stuart.krueger9@gmail.com>
2023-10-30 23:24:03 -0400

    defined canvas on client.js before canvas instantiation becaused it was causing an issue on another computer

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 let clientGame = ""
-//let canvas = document.getElementById("canvas1")
+let canvas = document.getElementById("canvas1")
 let localPlayer = ""
 const mouse = { x: undefined, y: undefined }
 
