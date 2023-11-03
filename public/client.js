c7b0570568e084c472292873cfb23433ee479380
kruegsw <stuart.krueger9@gmail.com>
2023-11-03 04:07:29 -0400

    forgot to point client to charlization server

--- public/client.js
+++ public/client.js
@@ -9,8 +9,8 @@ let localPlayer = ""
 const mouse = { x: undefined, y: undefined }
 
 //const socket = io("https://127.0.0.1:4000", {transports: ['websocket', 'polling']} )
-const socket = io("https://localhost:4000", {transports: ['websocket', 'polling']} )
-//const socket = io("https://charlization.com:4000", {transports: ['websocket', 'polling']} )
+//const socket = io("https://localhost:4000", {transports: ['websocket', 'polling']} )
+const socket = io("https://charlization.com:4000", {transports: ['websocket', 'polling']} )
 socket.on("connect", () => { console.log(`You are socket.id ${socket.id}`) })
 socket.on('init-client-game', serverGame => {
     clientGame = serverGame
